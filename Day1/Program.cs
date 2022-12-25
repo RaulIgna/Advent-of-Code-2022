1a1df7656c6b61b876ff9dd28bf967510c038bbe
RaulIgna <ignaraul12@yahoo.com>
2022-12-25 23:40:53 +0200

    Added Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,50 @@
+﻿string[] text = System.IO.File.ReadAllLines("@../../../../../input.in"); // Get all values from a file
+
+int[] value = new int[text.Length]; // Create array of int
+
+for(int i = 0; i < text.Length;i++)
+{
+    value[i] = Convert.ToInt32(text[i]); // Convert from string to int32
+}
+
+Array.Sort(value); // Sort them
+
+void Part1()
+{
+    foreach (int it in value)
+    {
+        foreach (int it2 in value)
+        {
+            if (it == it2) continue;
+            if (it + it2 == 2020)
+            {
+                Console.Write("First part: ");
+                Console.WriteLine(it * it2);
+                return;
+            }
+        }
+    }
+}
+
+void Part2()
+{
+    foreach (int it in value)
+    {
+        foreach (int it2 in value)
+        {
+            foreach (int it3 in value)
+            {
+                if (it == it2 || it2 == it3 || it == it3) continue;
+                if (it + it2 + it3 == 2020)
+                {
+                    Console.Write("Second part: ");
+                    Console.WriteLine(it * it2 * it3);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+Part1();
+Part2();
